36c357bbe0eabb737cb7a143326466865aba2054
SuhininKonstantinASU19bzu <65463165+SuhininKonstantinASU19bzu@users.noreply.github.com>
2022-05-19 15:07:53 +0500

    Final

---
+++ Lab_02/Blockchain.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using System.Linq;
+
+namespace Lab_02 {
+    [Serializable()]
+    class Block {
+        public Block(string ts, string dat, string hs, int nc) {
+            timestamp = ts;
+            data = dat;
+            hash = hs;
+            nonce = nc;
+        }
+
+        string timestamp;
+        string data;
+        string hash;
+        int nonce;
+
+        public string GetTimestamp => timestamp;
+        public string GetData => data;
+        public string GetHash => hash;
+        public int GetNonce => nonce;
+        
+        public string SetData {
+            get => data;
+            set => data = value;
+        }
+    }
+
+    class Blockchain {
+        static List<Block> blockchain = new List<Block>();
+        static int difficulity = 2;
+       
+        public static List<Block> GetBlockchain => blockchain;
+
+        #region Add
+
+        public static void AddBlock(string dat) {
+            string prvHash;
+            int nonce = 0;
+            string timestamp = Convert.ToString(DateTime.Now);
+
+            if (blockchain.Count == 0) {
+                prvHash = "";
+
+                while (true) {
+                    string newHash = getHash(timestamp, dat, prvHash, nonce);
+
+                    if (newHash.StartsWith(String.Concat(Enumerable.Repeat("0", difficulity)))) {
+                        Console.WriteLine("Найден блок {0}, nonce - {1}", newHash, nonce);
+
+                        blockchain.Add(new Block(timestamp, dat, newHash, nonce));
+
+                        break;
+                    }
+                    else nonce++;
+                }
+            }
+            else {
+                prvHash = blockchain.Last().GetHash;
+
+                while (true) {
+                    string newHash = getHash(timestamp, dat, prvHash, nonce);
+
+                    if (newHash.StartsWith(String.Concat(Enumerable.Repeat("0", difficulity)))) {
+                        Console.WriteLine("Найден блок {0}, nonce - {1}", newHash, nonce);
+
+                        blockchain.Add(new Block(timestamp, dat, newHash, nonce));
+
+                        break;
+                    }
+                    else nonce++;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Edit
+
+        public static void EditBlock(int block, string data) {
+            blockchain[block].SetData = data;
+        }
+
+        #endregion
+
+        #region Show
+
+        public static void Show() {
+            int i = 0;
+
+            foreach (Block blc in blockchain) {
+                Console.WriteLine("{0}, {1}, {2}, {3}, {4}", i, blc.GetData, blc.GetHash, blc.GetTimestamp, blc.GetNonce);
+                
+                i++;
+            }
+        }
+
+        #endregion
+
+        #region Verification
+
+        static string getHash(string ts, string dat, string prvHash, int nc) {
+            using (SHA256 hash = SHA256Managed.Create()) {
+                return String.Concat(hash
+                    .ComputeHash(Encoding.UTF8.GetBytes(ts + dat + prvHash + nc))
+                    .Select(item => item.ToString("x2"))); ;
+            }
+        }
+
+        public static void Verification() {
+            if (blockchain.Count != 0) {
+                for (int i = 1; i != blockchain.Count; i++) {
+                    if (getHash(blockchain[i].GetTimestamp,
+                                blockchain[i].GetData,
+                                blockchain[i - 1].GetHash,
+                                blockchain[i].GetNonce) == blockchain[i].GetHash) Console.WriteLine("Block {0} - OK", i);
+                    else {
+                        Console.WriteLine("Найдено несоответствие !!!");
+
+                        return;
+                    }
+
+                }
+
+                Console.WriteLine("Все блоки подтверждены успешно!!");
+            }
+        }
+
+        #endregion
+
+        #region File
+
+        static BinaryFormatter formatter = new BinaryFormatter();
+
+        public static void SaveToFile(string filename) {
+            using (FileStream fs = new FileStream(filename + ".amber", FileMode.Create)) {
+                formatter.Serialize(fs, blockchain);
+            }
+        }
+
+        public static void LoadFromFile(string filename) {
+            using (FileStream fs = new FileStream(filename + ".amber", FileMode.OpenOrCreate)) {
+                blockchain = (List<Block>)formatter.Deserialize(fs);
+
+                foreach (Block blc in blockchain) {
+                    Console.WriteLine("{0}, {1}, {2}, {3}", blc, blc.GetData, blc.GetHash, blc.GetTimestamp);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Lab_02/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace Lab_02 {
+    class Program {
+        #region Input
+
+        static string InputData(string message) {
+            Console.WriteLine(message);
+
+            Console.Write("> ");
+
+            return Console.ReadLine();
+        }
+
+        #endregion
+
+        #region Input Checkers
+
+        static uint NotNullInputCheck(string message)
+        {
+            bool input_first_scan = true;
+            uint input;
+
+            do
+            {
+                if (!input_first_scan)
+                {
+                    Console.WriteLine("\n!!!                     Ошибка ввода                    !!!\n" +
+                        "--- Допустим ввод в формате целого положительного числа ---" +
+                        "---                   Не равного нулю                   ---");
+                }
+                else input_first_scan = false;
+
+                Console.WriteLine(message);
+
+                Console.Write("> ");
+
+            } while (!uint.TryParse(Console.ReadLine(), out input) || input == 0);
+
+            return input;
+        }
+
+        static uint InputCheck(string message) {
+            bool input_first_scan = true;
+            uint input;
+
+            do {
+                if (!input_first_scan) {
+                    Console.WriteLine("\n!!!                     Ошибка ввода                    !!!\n" +
+                        "--- Допустим ввод в формате целого положительного числа ---");
+                }
+                else input_first_scan = false;
+
+                Console.WriteLine(message);
+
+                Console.Write("> ");
+
+            } while (!uint.TryParse(Console.ReadLine(), out input));
+
+            return input;
+        }
+
+        static uint InRangeInputCheck(int begin, int end, string message) {
+            bool input_first_scan = true;
+            uint input;
+
+            do {
+                if (!input_first_scan) {
+                    Console.WriteLine("\n!!!                     Ошибка ввода                    !!!\n" +
+                        "--- Допустим ввод в формате целого положительного числа ---\n" +
+                        "---                 Заданного диапазона                 ---");
+                }
+                else input_first_scan = false;
+
+                Console.WriteLine(
+                    message +
+                    "***                  В диапазоне от " +
+                    begin.ToString() +
+                    " до                ***\n" +
+                    "***                  " +
+                    end.ToString() +
+                    "                ***\n"
+                    );
+
+                Console.Write("> ");
+
+            } while (!uint.TryParse(Console.ReadLine(), out input) || input < begin || input > end);
+
+            return input;
+        }
+
+        #endregion
+
+        #region Menu
+
+        static void Menu() {
+            uint menu;
+
+            do {
+                menu = InputCheck(
+                    "\n***                    Основное меню                    ***\n" +
+                    "1. Создание нового блока\n" +
+                    "2. Вывод информации о ранее созданых блоках\n" +
+                    "3. Проверка всех созданных блоков\n" +
+                    "4. Изменение блока\n" +
+                    "5. Сохранение в файл\n" +
+                    "6. Загрузка из файла\n" +
+                    "0. Выход"
+                    );
+
+                switch (menu) {
+                    case 1:
+                        Blockchain.AddBlock(InputData("\n>>>                   Задайте данные                    <<<"));
+
+                        break;
+
+                    case 2:
+                        Blockchain.Show();
+
+                        break;
+
+                    case 3:
+                        Blockchain.Verification();
+
+                        break;
+
+                    case 4:
+                        if (Blockchain.GetBlockchain.Count != 0) {
+                            Blockchain.EditBlock(
+                            (int)InRangeInputCheck(0, Blockchain.GetBlockchain.Count - 1, "\n>>>          Задайте номер блока для изменени           <<<\n"),
+                            InputData("\n>>>                Задайте новые данные                 <<<"));
+                        }
+
+                        break;
+
+                    case 5:
+                        Blockchain.SaveToFile(InputData("\n>>>          Задайте имя файла для сохранения           <<<"));
+
+                        break;
+
+                    case 6:
+                        Blockchain.LoadFromFile(InputData("\n>>>           Задайте имя файла для загрузки            <<<"));
+
+                        break;
+                }
+
+            } while (menu != 0);
+        }
+
+        #endregion
+
+        static void Main(string[] args) {
+            Console.WriteLine("-----------------------------------------------------------\n" +
+        "---                Лабораторная работа 02               ---\n" +
+        "---                                                     ---\n" +
+        "-----------------------------------------------------------");
+
+            Menu();
+        }
+    }
+}
